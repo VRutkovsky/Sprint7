301acb81ef1e65649cad2316d80088a330ebb1d2
Vladimir Rutkovskiy <v-rutkovsky@yandex.ru>
2022-08-31 09:20:29 +0300

    Update 1

--- src/test/java/org/example/scouter/CourierCreateTest.java
+++ src/test/java/org/example/scouter/CourierCreateTest.java
@@ -4,35 +4,46 @@ import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.apache.http.HttpStatus.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class ScouterCourierTest {
+public class CourierCreateTest {
+    private Courier courier;
+    private ValidatableResponse response;
 
-    // Positive Test
-    // Courier created successfully
     @Before
     public void setUp() {
         RestAssured.baseURI = ScouterAPI.apiBasicURL;
     }
+
+    @After
+    public void cleanUp() {
+        //Test data clean-up
+        response = courier.apiDelete();
+    }
+
+        // Positive Test
+    // Courier created successfully
 
     @Test
     @DisplayName("Create Courier. Happy path")
     @Description("Data provided, response code 201, Login successful")
     public void courierCreateSuccessTest() {
         // Test data initialization
-        Courier courier = new Courier();
+        courier = new Courier();
 
         // Test run
         ValidatableResponse response = courier.apiCreate();
-        System.out.println(response.extract().body().asString());
+//        System.out.println(response.extract().body().asString());
 
         //Status Code check
         int statusCode = response.extract().statusCode();
-        assertTrue("Courier creation failed. Response code is incorrect",statusCode == 201);
+        assertTrue("Courier creation failed. Response code is incorrect",statusCode == SC_CREATED);
 
         //Response body check
         boolean isCourierCreated = response.extract().path("ok");
@@ -40,13 +51,10 @@ public class ScouterCourierTest {
 
         //Courier verification via login
         response = courier.apiLogin();
-        System.out.println(response.extract().body().asString());
+//        System.out.println(response.extract().body().asString());
 
-        assertTrue("Courier creation is not successful. Login attempt is failed.", response.extract().statusCode() == 200);
+        assertTrue("Courier creation is not successful. Login attempt is failed.", response.extract().statusCode() == SC_OK);
 
-        //Test data clean-up
-        response = courier.apiDelete();
-        System.out.println(response.extract().body().asString());
     }
 
     // Negative Tests
@@ -60,19 +68,19 @@ public class ScouterCourierTest {
 
         // Test run - create courier first time
         ValidatableResponse response = courier.apiCreate();
-        System.out.println(response.extract().body().asString());
+//        System.out.println(response.extract().body().asString());
 
         //Make sure, that courier is created
         int statusCode = response.extract().statusCode();
-        assertTrue("Courier creation failed. Response code is incorrect",statusCode == 201);
+        assertTrue("Courier creation failed. Response code is incorrect",statusCode == SC_CREATED);
 
         // Test run - create courier second time
         response = courier.apiCreate();
-        System.out.println(response.extract().body().asString());
+//        System.out.println(response.extract().body().asString());
 
         //Check Status Code
         statusCode = response.extract().statusCode();
-        assertTrue("Courier duplication check failed. Response code is incorrect",statusCode == 409);
+        assertTrue("Courier duplication check failed. Response code is incorrect",statusCode == SC_CONFLICT);
 
         //Check Response body
         String strExpected = "Этот логин уже используется";
@@ -81,7 +89,7 @@ public class ScouterCourierTest {
 
         //Test data clean-up
         response = courier.apiDelete();
-        System.out.println(response.extract().body().asString());
+//        System.out.println(response.extract().body().asString());
     }
 
     // Courier Data not enough: Login
@@ -94,11 +102,11 @@ public class ScouterCourierTest {
 
         // Test run
         ValidatableResponse response = courier.apiCreate(true);
-        System.out.println(response.extract().body().asString());
+//        System.out.println(response.extract().body().asString());
 
         //Status Code check
         int statusCode = response.extract().statusCode();
-        assertTrue("Courier rejection failed. Response code is incorrect",statusCode == 400);
+        assertTrue("Courier rejection failed. Response code is incorrect",statusCode == SC_BAD_REQUEST);
 
         //Response body check
         String strExpected = "Недостаточно данных для создания учетной записи";
@@ -116,11 +124,11 @@ public class ScouterCourierTest {
 
         // Test run
         ValidatableResponse response = courier.apiCreate(true);
-        System.out.println(response.extract().body().asString());
+//        System.out.println(response.extract().body().asString());
 
         //Status Code check
         int statusCode = response.extract().statusCode();
-        assertTrue("Courier rejection failed. Response code is incorrect",statusCode == 400);
+        assertTrue("Courier rejection failed. Response code is incorrect",statusCode == SC_BAD_REQUEST);
 
         //Response body check
         String strExpected = "Недостаточно данных для создания учетной записи";
@@ -138,11 +146,11 @@ public class ScouterCourierTest {
 
         // Test run
         ValidatableResponse response = courier.apiCreate(true);
-        System.out.println(response.extract().body().asString());
+//        System.out.println(response.extract().body().asString());
 
         //Status Code check
         int statusCode = response.extract().statusCode();
-        assertTrue("Courier creation failed. Response code is incorrect",statusCode == 201);
+        assertTrue("Courier creation failed. Response code is incorrect",statusCode == SC_CREATED);
 
         //Response body check
         boolean isCourierCreated = response.extract().path("ok");
@@ -150,7 +158,7 @@ public class ScouterCourierTest {
 
         //Test data clean-up
         response = courier.apiDelete();
-        System.out.println(response.extract().body().asString());
+//        System.out.println(response.extract().body().asString());
     }
     // Courier Data not enough: no data
     @Test
@@ -162,11 +170,11 @@ public class ScouterCourierTest {
 
         // Test run
         ValidatableResponse response = courier.apiCreate(true);
-        System.out.println(response.extract().body().asString());
+//        System.out.println(response.extract().body().asString());
 
         //Status Code check
         int statusCode = response.extract().statusCode();
-        assertTrue("Courier rejection failed. Response code is incorrect", statusCode == 400);
+        assertTrue("Courier rejection failed. Response code is incorrect", statusCode == SC_BAD_REQUEST);
 
         //Response body check
         String strExpected = "Недостаточно данных для создания учетной записи";
--- src/test/java/org/example/scouter/CourierLoginTest.java
+++ src/test/java/org/example/scouter/CourierLoginTest.java
@@ -1,2 +1,128 @@
-package org.example.scouter;public class CourierLoginTest {
+package org.example.scouter;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.apache.http.HttpStatus.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CourierLoginTest {
+    private ValidatableResponse response;
+    private Courier courier;
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = ScouterAPI.apiBasicURL;
+
+        // Test data initialization
+        courier = new Courier();
+
+        // Create test courier
+        response = courier.apiCreate();
+//        System.out.println(response.extract().body().asString());
+
+        //Status Code check
+        int statusCode = response.extract().statusCode();
+        assertEquals("Courier creation failed. Response code is incorrect",SC_CREATED , statusCode);
+    }
+
+    @After
+    public void cleanUp(){
+        //Test data clean-up
+        response = courier.apiDelete();
+//        System.out.println(response.extract().body().asString());
+    }
+
+    //Positive Test
+    //Courier Logged in successfully. Courier ID is returned.
+    @Test
+    @DisplayName("Courier login. Happy path")
+    @Description("Data provided, response code 200, Courier ID is greater zero")
+    public void courierLoginSuccessTest() {
+
+        response = courier.apiLogin();
+//        System.out.println(response.extract().body().asString());
+
+        assertEquals("Courier login is not successful.", SC_OK, response.extract().statusCode());
+
+        int id = response.extract().path("id");
+        assertTrue("", id > 0);
+    }
+
+
+    //Negative Test
+    //No login provided
+    @Test
+    @DisplayName("Courier login. No login")
+    @Description("Login not provided, response code 400")
+    public void courierNoLoginTest() {
+
+        response = courier.apiLogin(null, courier.getPassword());
+//        System.out.println(response.extract().body().asString());
+
+        assertEquals("Courier login rejection failed.", SC_BAD_REQUEST, response.extract().statusCode());
+
+        //Response body check
+        String strExpected = "Недостаточно данных для входа";
+        String strResponse = response.extract().path("message");
+        assertEquals("Courier login rejection failed. Body content is incorrect", strExpected, strResponse );
+
+    }
+
+    //No password provided
+    @Test
+    @DisplayName("Courier login. No password")
+    @Description("No password provided, response code 400")
+    public void courierNoPasswordTest() {
+
+        response = courier.apiLogin(courier.getLogin(), null);
+//        System.out.println(response.extract().body().asString());
+
+        assertEquals("Courier login rejection failed.", SC_BAD_REQUEST, response.extract().statusCode());
+
+        //Response body check
+        String strExpected = "Недостаточно данных для входа";
+        String strResponse = response.extract().path("message");
+        assertEquals("Courier login rejection failed. Body content is incorrect", strExpected, strResponse);
+    }
+
+    //Wrong Login provided
+    @Test
+    @DisplayName("Courier login. Wrong login")
+    @Description("Wrong Login provided, response code 404")
+    public void courierWrongLoginTest() {
+
+        response = courier.apiLogin("bfgbvpoyjjnyhn", courier.getPassword());
+//        System.out.println(response.extract().body().asString());
+
+        assertEquals("Courier login rejection failed.", SC_NOT_FOUND, response.extract().statusCode());
+
+        //Response body check
+        String strExpected = "Учетная запись не найдена";
+        String strResponse = response.extract().path("message");
+        assertEquals("Courier login rejection failed. Body content is incorrect", strExpected, strResponse );
+    }
+
+    //Wrong password provided
+    @Test
+    @DisplayName("Courier login. Wrong password")
+    @Description("Wrong password provided, response code 404")
+    public void courierWrongPasswordTest() {
+
+        response = courier.apiLogin(courier.getLogin(), "wgf67z4v68cs");
+//        System.out.println(response.extract().body().asString());
+
+        assertEquals("Courier login rejection failed.", SC_NOT_FOUND, response.extract().statusCode());
+
+        //Response body check
+        String strExpected = "Учетная запись не найдена";
+        String strResponse = response.extract().path("message");
+        assertEquals("Courier login rejection failed. Body content is incorrect", strExpected, strResponse );
+    }
 }
